40a8c47daffa61d9aae50a1770ea735b8425a985
jesbram <jesus26abraham1996@gmail.com>
2021-09-06 22:53:41 -0400

    update

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from jsonfield import JSONField
+from django.contrib.postgres.fields import JSONField
 from datetime import datetime
 # Create your models here.
 class Theme(models.Model):
